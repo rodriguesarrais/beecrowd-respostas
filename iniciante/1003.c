b635feb461280f85a56de0fe03455f5b57348e69
Arrais <134543499+rodriguesarrais@users.noreply.github.com>
2023-10-26 20:56:46 -0300

    1003, 1004 e 1005

---
+++ iniciante/1003.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int A;
+    int B;
+    
+    scanf("%i", &A);
+    scanf("%i", &B);
+    
+    int SOMA = A + B;
+    
+    printf("SOMA = %i\n", SOMA);
+    
+    
+ 
+    return 0;
+}
---
+++ iniciante/1004.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int a;
+    int b;
+    
+    scanf("%i", &a);
+    scanf("%i", &b);
+    
+    int PROD = a*b;
+    
+    printf("PROD = %i\n", PROD);
+ 
+    return 0;
+}
